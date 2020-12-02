4b5622226b340fee2db7942993e1e4baed5497de
Jérémy Tapia <jeremy.tapia30@hotmail.com>
2020-12-02 01:17:01 +0100

    Après avoir tester les arbres

---
+++ Assets/Scripts/UI/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    private static GameManager _instance;
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+}
